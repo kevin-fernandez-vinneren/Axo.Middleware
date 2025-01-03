37dc9afdf398c8491ef58a394cf40298f11913b1
kevin-fernandez-vinneren <kevin@vinneren.com.mx>
2025-01-03 14:13:42 -0600

    Remove unused imports from Function.cs.
    Cleaned up unused imports to improve code readability and maintainability. The removed imports were not required for the current implementation and their presence could lead to confusion or unnecessary dependency management.

--- src/Axo.Middleware.Functions/Axo.Functions.FilesTrigger/src/Axo.Functions.FilesTrigger/Function.cs
+++ src/Axo.Middleware.Functions/Axo.Functions.FilesTrigger/src/Axo.Functions.FilesTrigger/Function.cs
@@ -1,7 +1,4 @@
 using Amazon.Lambda.Core;
-using Amazon.Lambda.RuntimeSupport;
-using Amazon.Lambda.Serialization.SystemTextJson;
-using System.Text.Json.Serialization;
 using Amazon.Lambda.Annotations;
 using Amazon.Lambda.S3Events;
 using Logify.Extensions;
